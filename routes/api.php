71053e81c7922256cc1f823db02f8d1537de4df7
shayan <shayan3k@gmail.com>
2021-11-26 01:55:54 +0330

    first commit

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::get('/bot/getupdates', function() {
+    $updates = Telegram::getUpdates();
+    return (json_encode($updates));
+});
+
+
+Route::get('bot/sendmessage', function() {
+    Telegram::sendMessage([
+        'chat_id' => env('TELEGRAM_RECIPIENT_CHAT_ID', ""),
+        'text' => 'Hello world!'
+    ]);
+    return;
+});
+
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
